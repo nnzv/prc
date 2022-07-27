8d94ab6632063ac7727405c48eef78f010aadf39
Enzo Venturi <zventuri@tuta.io>
2022-07-27 11:29:09 -0500

    doc: add overview

--- prc.go
+++ prc.go
@@ -1,1 +1,26 @@
+/*
+Package prc offers functions to data lookups for files stored in 
+the pseudo-filesystem proc. 
+
+Example
+
+Here’s a trivial example that gives current uptime of the system:
+
+    package main
+
+    import (
+        "fmt"
+        "log"
+
+        "github.com/nnzv/prc"
+    )
+
+    func main() {
+        age, err := prc.Uptime()
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("Uptime is %s\n", age)
+    }
+*/
 package prc
