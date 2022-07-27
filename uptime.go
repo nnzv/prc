69962dee3509cd354686292485c91e3f346f6c41
Enzo Venturi <zventuri@tuta.io>
2022-07-27 11:37:50 -0500

    doc: add Uptime explain

--- uptime.go
+++ uptime.go
@@ -5,6 +5,9 @@ import (
     "golang.org/x/sys/unix"
 )
 
+// Uptime returns uptime of the system (including time spent in suspend) and the
+// amount of time spent in the idle process. Since age is time duration, you can
+// use its type methods related.
 func Uptime() (age time.Duration, err error) {
     var sys unix.Sysinfo_t
     err = unix.Sysinfo(&sys)
