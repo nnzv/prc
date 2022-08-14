a79402b81f0ea7460d5a8a7eb327a69a07b74d3a
Enzo Venturi <zventuri@tuta.io>
2022-08-14 18:35:01 -0500

    util.go: initial commit

---
+++ util.go
@@ -0,0 +1,22 @@
+package prc
+
+import (
+    "log"
+    "strconv"
+)
+
+func Atoi(str string) (i int) {
+    i, err := strconv.Atoi(str)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return i
+}
+
+func U64(str string) (u uint64) {
+    u, err := strconv.ParseUint(str, 10, 32)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return u
+}
