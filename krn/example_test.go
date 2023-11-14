d8d101ebe8ce4e172faf66ed070dc9f290469a4f
Enzo Venturi <nzventuri@proton.me>
2023-11-13 20:18:28 -0500

    krn: i forgot to add some license headers

--- krn/example_test.go
+++ krn/example_test.go
@@ -1,3 +1,7 @@
+// Copyright 2023 Enzo Venturi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package krn_test
 
 import (
