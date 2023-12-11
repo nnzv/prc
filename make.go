3ae31331c8f54aab138971b9b117a5c14d82fdda
nzv <10971441-nzv@users.noreply.gitlab.com>
2023-12-11 01:04:18 -0500

    prc: drop Makefile, use a primitive make.go

---
+++ make.go
@@ -0,0 +1,68 @@
+// Copyright 2023 Enzo Venturi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build ignore
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+var out bytes.Buffer
+var dir, target string // flags
+
+func init() {
+	flag.StringVar(&dir, "dir", "./...", "specify the directory for Go commands")
+	flag.StringVar(&target, "target", "test", "specify the make target")
+	flag.Usage = func() {
+		log.Println("usage of make: ")
+		flag.PrintDefaults()
+	}
+	log.SetFlags(0)
+}
+
+func main() {
+	flag.Parse()
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch target {
+	case "test":
+		run("go", "test", "-v", "-count=1", dir)
+	case "site":
+		run("pkgsite", "-open")
+	case "report":
+		run("go", "test", "-run=TestGenerateReport")
+	case "fmt":
+		run("gofmt", "-w", "-s", ".")
+	case "check":
+		run("gofmt", "-l", ".")
+		if out.Len() > 0 {
+			os.Exit(1)
+		}
+		run("go", "vet", dir)
+	default:
+		log.Fatalf("make: unknown target %#v\n", target)
+	}
+}
+
+func run(cmd string, args ...string) {
+	c := exec.Command(cmd, args...)
+	log.Println(c.String())
+	c.Stdout = &out
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		log.Fatalf("make: %s", err)
+	}
+	if out.Len() > 0 {
+		log.Print(&out)
+	}
+}
