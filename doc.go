e2a3a14d5038de84a5ae0923992e92f4f0986126
nzv <10971441-nzv@users.noreply.gitlab.com>
2023-12-16 07:53:54 -0500

    doc.go: add note about build contraints

--- doc.go
+++ doc.go
@@ -7,6 +7,11 @@
 /*
 Package prc parses [procfs] files on Linux systems.
 
+It's important to note that each go file in the  project is specifically configured to
+build exclusively for Linux. In the event that the GOOS variable is set to a value
+other than "linux" during interactions with the "go build" command, accessibility to the
+functions in this package might be restricted due to the platform constraint.
+
 [procfs]: https://www.kernel.org/doc/man-pages/online/pages/man5/proc.5.html
 */
 package prc
